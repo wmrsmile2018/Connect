bc874afc5c0d71cca15b6dc383b8c40806dacc34
Tien <lodkat01@gmail.com>
2019-07-10 20:13:59 +0300

    finished button sign up

--- js/buttons_main_page.js
+++ js/buttons_main_page.js
@@ -2,6 +2,7 @@ var flag_sign = 0;
 var flag_dd = 0;
 var flag_mm = 0;
 var flag_yyyy = 0;
+var flag_modal_sign = 0;
 var month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
 $(function() {
 
@@ -35,9 +36,22 @@ $(function() {
     }
   }
 
+  function sign_but_2 () {
+    if ($(window).scrollTop() >= 44 && flag_sign == 0) {
+      flag_sign = 1;
+      $(".sign_2").toggleClass("open");
+      return 1;
+    } else if ($(window).scrollTop() < 44 && flag_sign == 1) {
+      flag_sign = 0;
+      $(".sign_2").toggleClass("open");
+      return 0;
+    }
+  }
+
   years_Li();
   day_Li();
   month_Li();
+  sign_but_2();
 
   $(".text_8").on("mouseover", ".h_inf", (e) => {
     $(".but_inf").css("fill", "#0e94ff");
@@ -105,16 +119,44 @@ $(function() {
   });
 
   $(window).scroll((e) => {
-    if ($(window).scrollTop() >= 44 && flag_sign == 0) {
-      flag_sign = 1;
-      $(".sign_2").toggleClass("open");
-    } else if ($(window).scrollTop() < 44 && flag_sign == 1) {
-      flag_sign = 0;
-      $(".sign_2").toggleClass("open");
+    sign_but_2();
+  });
+
+  $(window).click((e) => {
+    var svg1 = $(".but_sign_up_1");
+    var svg2 = $(".but_sign_up_2");
+    var pol1 = $(".but_sign_up_1 polygon");
+    var pol2 = $(".but_sign_up_2 polygon");
+    var h1 = $(".h_sign_up_1");
+    var h2 = $(".h_sign_up_2");
+
+    if(((svg1.is(e.target) || svg2.is(e.target)) ||
+        (pol1.is(e.target) || pol2.is(e.target)) ||
+        (h1.is(e.target) || h2.is(e.target)) && flag_modal_sign == 0) ) {
+      console.log("hello1");
+      flag_modal_sign = 1;
+      $(".sign_window").toggleClass("open");
+      if($(window).scrollTop() >= 44) {
+        flag_sign = 0;
+        $(".sign_2").toggleClass("open");
+      }
     }
   });
 
-  $(".selector_table_dd").on("click", ".dd_dropdown", (e) => {
+  $(document).mouseup(function(e) {
+    var div = $(".sign_window");
+    if (!div.is(e.target) &&
+      div.has(e.target).length === 0 && flag_modal_sign == 1) {
+      flag_modal_sign = 0;
+      div.toggleClass("open");
+      if($(window).scrollTop() >= 44) {
+        flag_sign = 1;
+        $(".sign_2").toggleClass("open");
+      }
+    }
+  });
+
+  $(".selector_table_dd").click((e) => {
     if (flag_dd == 0) {
       flag_dd = 1;
       flag_yyyy = 0;
@@ -149,13 +191,13 @@ $(function() {
     if (index) {
       $(".selector_input_dd").val(index);
     } else {
-      $(".selector_input_dd").val("День");
+      $(".selector_input_dd").val("");
     }
     $(".dd_container").hide();
     $(".svg_cont_dd").hide();
   });
 
-  $(".selector_table_mm").on("click", ".mm_dropdown", (e) => {
+  $(".selector_table_mm").click((e) => {
     if (flag_mm == 0) {
       flag_mm = 1;
       flag_yyyy = 0;
@@ -188,15 +230,15 @@ $(function() {
   $(".month_el").click((e) => {
     var index = $(e.currentTarget).data("index");
     if (index) {
-      $(".selector_input_mm").val(index);
+      $(".selector_input_mm").val(month[index - 1]);
     } else {
-      $(".selector_input_mm").val("Месяц");
+      $(".selector_input_mm").val("");
     }
     $(".mm_container").hide();
     $(".svg_cont_mm").hide();
   });
 
-  $(".selector_table_yyyy").on("click", ".yyyy_dropdown", (e) => {
+  $(".selector_table_yyyy").click((e) => {
     if (flag_yyyy == 0) {
       flag_yyyy = 1;
       flag_dd = 0;
@@ -219,6 +261,7 @@ $(function() {
       div.hide();
       $(".svg_cont_yyyy").hide();
     }
+
   });
 
   $(".list_yyyy li").hover(function() {
@@ -226,12 +269,12 @@ $(function() {
   }, function() {
     $(this).removeClass("years_el_hover");
   });
-  $(".month_el").click((e) => {
+  $(".years_el").click((e) => {
     var index = $(e.currentTarget).data("index");
     if (index) {
-      $(".selector_input_yyyy").val(index);
+      $(".selector_input_yyyy").val(index + 1500);
     } else {
-      $(".selector_input_yyyy").val("Год");
+      $(".selector_input_yyyy").val("");
     }
     $(".yyyy_container").hide();
     $(".svg_cont_yyyy").hide();
