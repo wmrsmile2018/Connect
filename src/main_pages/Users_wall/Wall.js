86b91e4d37d66494667f5c6ca3ded78ab4dd9ef8
Tien <lodkat01@gmail.com>
2019-08-23 19:40:34 +0300

    wall_projects step 1

--- src/main_pages/Users_wall/Wall.js
+++ src/main_pages/Users_wall/Wall.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './Wall.css'
+import W_projects from './Wall_Projects/W_projects'
 
 const Wall = () => {
   return (
     <div className="Wall">
-      
+      <W_projects />
     </div>
   );
 }
---
+++ src/main_pages/Users_wall/Wall_Projects/W_projects.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './W_projects.css';
+import placeholder from './../../../images/w_project_placeholder/placeholder_proj.png';
+
+let projects = [
+  {projectsClass: "project1" ,imgClass: "img1"},
+  {projectsClass: "project2" ,imgClass: "img2"},
+  {projectsClass: "project3" ,imgClass: "img3"},
+  {projectsClass: "project1" ,imgClass: "img1"},
+  {projectsClass: "project2" ,imgClass: "img2"},
+  {projectsClass: "project3" ,imgClass: "img3"},
+  {projectsClass: "project1" ,imgClass: "img1"},
+  {projectsClass: "project2" ,imgClass: "img2"},
+  {projectsClass: "project3" ,imgClass: "img3"},
+  {projectsClass: "project4" ,imgClass: "img4"}
+]
+
+let elementsProject = projects.map((project) =>
+  <div className={"projects"  + projects.indexOf(project) % 2 + " " + project.projectsClass}>
+        <img src={placeholder} className={project.imgClass}/>
+        <div className="footerProject">
+          <div className="wrap_footerProject">
+            <p className="projectName"> Project Name </p>
+            <p className="author"> Author </p>
+            <p className="date"> 20.10.2019 </p>
+          </div>
+        </div>
+  </div>
+);
+
+const W_projects = () => {
+  return (
+    <div className="W_projects">
+        {elementsProject}
+    </div>
+  );
+}
+
+
+export default W_projects;
